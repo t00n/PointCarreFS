0b9c56d6e19d1cfd15a00d7d7c5c721f5a213ad2
t00n <kelkununtel@hotmail.com>
2015-10-23 16:36:03 +0200

    [fix] dotfile working

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os, imp
 
 NETID = ""
 PASSWORD = ""
-DOTFILE = "~/.pointcarrefs/local_config.py"
+DOTFILE = os.path.expanduser("~/.pointcarrefs/local_config.py")
 
 try:
     from local_config import *  # pragma: no flakes
 except ImportError:
-    NETID = imp.load_source("local_config.NETID", os.path.expanduser(DOTFILE))
-    PASSWORD = imp.load_source("local_config.PASSWORD", os.path.expanduser(DOTFILE))
+    NETID = imp.load_source("local_config", DOTFILE).NETID
+    PASSWORD = imp.load_source("local_config", DOTFILE).PASSWORD
