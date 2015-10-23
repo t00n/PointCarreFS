fdf7e3e56aad914dd1cfd4752971856de675a094
t00n <kelkununtel@hotmail.com>
2015-10-23 16:09:10 +0200

    [fix] get config from .pointcarrefs

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+import os, imp
+
 NETID = ""
 PASSWORD = ""
+DOTFILE = "~/.pointcarrefs/local_config.py"
 
 try:
     from local_config import *  # pragma: no flakes
 except ImportError:
-    pass
+    NETID = imp.load_source("local_config.NETID", os.path.expanduser(DOTFILE))
+    PASSWORD = imp.load_source("local_config.PASSWORD", os.path.expanduser(DOTFILE))
